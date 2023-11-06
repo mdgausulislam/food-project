2fa82cabdb18c347786e3ef265cfc198c5169637
unknown <gausulislam5@gmail.com>
2023-11-06 22:43:01 +0600

    update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo/logo.svg';
+import menubar from '../assets/icon/menu-01.svg'
+import sun from '../assets/icon/sun-01.svg'
+import nintendo from '../assets/icon/nintendo-switch.svg'
+import frame from '../assets/icon/Frame.svg'
 
 const NavBar = () => {
     return (
@@ -11,19 +15,16 @@ const NavBar = () => {
                 <div className="form-control">
                     <div className="input-group">
                         <input type="text" placeholder="Search Best Food" className="input input-bordered hidden lg:flex" />
-                        <button className="btn btn-square">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        <button className=" ps-2 btn-square border-[#e655271a] bg-[#e655271a]">
+                            <img src={frame} alt="" />
                         </button>
                     </div>
                 </div>
             </div>
-            <div className="navbar-end">
-                <a className="btn hidden sm:flex">Button</a>
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                </div>
+            <div className="navbar-end space-x-6">
+                <a className="border-2 rounded-md border-[#e655271a] hidden sm:flex bg-[#e655271a] p-[6px] "><img src={sun} alt="" /></a>
+                <a className="border-2 rounded-md border-[#e655271a] hidden sm:flex bg-[#e655271a] p-[5px] "><img src={nintendo} alt="" /></a>
+                <a className="border-2 rounded-md hidden sm:flex bg-[#1010001a] p-[6px] "><img src={menubar} alt="" /></a>
             </div>
         </div>
     );
